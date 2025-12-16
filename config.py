4123839d078a95353fc2cf3c4416cee8e97d2b7d
junz-debug <80238048+junz-debug@users.noreply.github.com>
2025-12-16 20:39:46 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# normalizing flow network hyperparameters. This Code is based on differnet, source code: https://github.com/marco-rudolph/differnet
+clamp_alpha = 3 
+n_coupling_blocks = 2
+fc_internal = 4096 
+dropout = 0
+n_feat = 1024
+
